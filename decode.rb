f393ff686e98f9e0a5984b7d519f104b4f555b89
Amkam <amaduskamara@gmail.com>
2022-02-24 14:46:52 +0000

    Add decode_char and decode_word methods

--- decode.rb
+++ decode.rb
@@ -1,5 +1,74 @@
 # decode_char method
+def decode_char(ch)
+  case ch
+  when '.-'
+    'A'
+  when '-...'
+    'B'
+  when '-.-.'
+    'C'
+  when '-..'
+    'D'
+  when '.'
+    'E'
+  when '..-.'
+    'F'
+  when '--.'
+    'G'
+  when '....'
+    'H'
+  when '..'
+    'I'
+  when '.---'
+    'J'
+  when '.-.'
+    'K'
+  when '.-..'
+    'L'
+  when '--'
+    'M'
+  when '-.'
+    'N'
+  when '---'
+    'O'
+  when '.--.'
+    'P'
+  when '--.-'
+    'Q'
+  when '.-.'
+    'R'
+  when '...'
+    'S'
+  when '-'
+    'T'
+  when '..-'
+    'U'
+  when '...-'
+    'V'
+  when '.--'
+    'W'
+  when '-..-'
+    'X'
+  when '-.--'
+    'Y'
+  when '--..'
+    'Z'
+  else
+    ch
+  end
+end
+
+puts decode_char(".-")
 
 # decode_word method
+def decode_word(str)
+  res = ''
+  str.split.each do |item|
+    res += decode_char(item)
+  end
+  res
+end
+
+puts decode_word("-- -.--")
 
 # decode method
